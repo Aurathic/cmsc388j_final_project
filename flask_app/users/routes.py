688c19c23f5da49c2f08aa08e6366dfa3752c97a
Adam <39442291+Aurathic@users.noreply.github.com>
2023-05-07 22:48:47 -0400

    Copy over basic templates from P5

--- flask_app/users/routes.py
+++ flask_app/users/routes.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, redirect, url_for, render_template, flash, request
 from flask_login import current_user, login_required, login_user, logout_user
 
+from ..forms import RegistrationForm, LoginForm
 from .. import bcrypt
+
 # from ..forms import RegistrationForm, LoginForm, UpdateUsernameForm
 from ..models import User
 
-users = Blueprint('users', __name__)
+users = Blueprint("users", __name__)
 
+"""
 @users.route("/register", methods=['GET', 'POST'])
 def register():
     pass
@@ -19,3 +22,50 @@ def login():
 @login_required
 def logout():
     pass
+"""
+
+users = Blueprint("users", __name__)
+
+
+@users.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("movies.index"))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(username=form.username.data, email=form.email.data, password=hashed)
+        user.save()
+
+        return redirect(url_for("users.login"))
+
+    return render_template("register.html", title="Register", form=form)
+
+
+@users.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("movies.index"))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.objects(username=form.username.data).first()
+
+        if user is not None and bcrypt.check_password_hash(
+            user.password, form.password.data
+        ):
+            login_user(user)
+            return redirect(url_for("users.account"))
+        else:
+            flash("Login failed. Check your username and/or password")
+            return redirect(url_for("users.login"))
+
+    return render_template("login.html", title="Login", form=form)
+
+
+@users.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("movies.index"))
